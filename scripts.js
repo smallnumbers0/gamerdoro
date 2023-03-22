c0a8cad36e786ef649b8290bd867359f8c5f4538
Jacky <jchoi53@mail.ccsf.edu>
2023-03-22 08:44:08 -0700

    add Kay study timmer

--- scripts.js
+++ scripts.js
@@ -1,13 +1,85 @@
+
+
 document.querySelector('.hide-right').addEventListener('click', hideRightNav)
 document.querySelector('.hide-left').addEventListener('click', hideLeftNav)
 
 function hideRightNav() {
     document.querySelector('.contributors-list').classList.toggle('change')
-    document.querySelector('.hide-right').style.opacity = '1'
 }
 
 function hideLeftNav() {
     document.querySelector('.link-list').classList.toggle('change')
-    document.querySelector('.hide-right').style.opacity = '1'
 }
 
+/***************************************** TIMER CODE *****************************************/
+
+// Base times
+
+const studyDuration = 50
+const gameDuration = 25
+const plusFiveMinutes = 5
+let studyCounter = 0
+let gamingCounter = 0
+
+// Buttons
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
+const breakButton = document.querySelector('#break')
+const plusButton = document.querySelector('#plus')
+const finishButton = document.querySelector('#finish')
+const resetButton = document.querySelector('#reset')
+const countdownDisplay = document.querySelector('#countdown')
+const studyCount = document.querySelector('#work-count')
+const gamingCount = document.querySelector('#game-count')
+
+/***************************************** CHANGE ALL FUNCTIONS TO ANONYMOUS FUNCTIONS *****************************************/
+
+// Start Button event listener and function
+startButton.addEventListener('click', startStudyTimer)
+
+function startStudyTimer() {
+    let totalStudyTime = studyDuration * 60
+
+    let timerInterval = setInterval(function() {
+        let displaySeconds = (totalStudyTime % 60).toString().padStart(2, '0')
+        let displayMinutes = (Math.floor(totalStudyTime / 60)).toString().padStart(2, '0')
+
+        countdownDisplay.innerHTML = `${displayMinutes} : ${displaySeconds}`
+
+        totalStudyTime--
+
+        if (totalStudyTime < 0) {
+            studyCounter++
+            studyCount.innerHTML = `Work Sessions: ${studyCounter}`
+        }
+
+        resetButton.addEventListener('click', event => {
+            clearInterval(timerInterval)
+        })
+    }, 1000)
+}
+
+// Pause Button event listener and function
+pauseButton.addEventListener('click', pauseTimer)
+
+function pauseTimer() {}
+
+// Break Button event listener and function
+breakButton.addEventListener('click', startGameTimer)
+
+function startGameTimer() {}
+
+// Plus 5 event listener and function
+plusButton.addEventListener('click', addFiveToTimer)
+
+function addFiveToTimer() {}
+
+// Finish Button event listener and function
+finishButton.addEventListener('click', endTimer)
+
+function endTimer() {}
+
+// Reset Timer event listener and function
+resetButton.addEventListener('click', resetTimer)
+
+function resetTimer() {}
