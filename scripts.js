81511f4d532b94786d59cdcff0217a5652df17dd
unknown <khulood.hassan@hotmail.com>
2023-03-22 07:09:19 +0400

    Foundation code

--- scripts.js
+++ scripts.js
@@ -11,3 +11,42 @@ function hideLeftNav() {
     document.querySelector('.link-list').classList.toggle('change')
 }
 
+// Buttons
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
+const breakButton = document.querySelector('#break')
+const plusButton = document.querySelector('#plus')
+const finishButton = document.querySelector('#finish')
+const resetButton = document.querySelector('#reset')
+
+/***************************************** CHANGE ALL FUNCTIONS TO ANONYMOUS FUNCTIONS *****************************************/
+
+// Start Button event listener and function
+startButton.addEventListener('click', startStudyTimer)
+
+function startStudyTimer() {}
+
+// Pause Button event listener and function
+pauseButton.addEventListener('click', pauseTimer)
+
+function pauseTimer() {}
+
+// Break Button event listener and function
+breakButton.addEventListener('click', startGameTimer)
+
+function startGameTimer() {}
+
+// Plus 5 event listener and function
+plusButton.addEventListener('click', addFiveToTimer)
+
+function addFiveToTimer() {}
+
+// Finish Button event listener and function
+finishButton.addEventListener('click', endTimer)
+
+function endTimer() {}
+
+// Reset Timer event listener and function
+resetButton.addEventListener('click', resetTimer)
+
+function resetTimer() {}
