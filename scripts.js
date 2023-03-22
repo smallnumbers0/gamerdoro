79ffacbfa9f71ac7b05bc0f67118acb17b9a5942
Jacky <jchoi53@mail.ccsf.edu>
2023-03-21 20:26:03 -0700

    make nav buttons disappear when shrunken down

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ document.querySelector('.hide-left').addEventListener('click', hideLeftNav)
 
 function hideRightNav() {
     document.querySelector('.contributors-list').classList.toggle('change')
+    document.querySelector('.hide-right').style.opacity = '1'
 }
 
 function hideLeftNav() {
     document.querySelector('.link-list').classList.toggle('change')
+    document.querySelector('.hide-right').style.opacity = '1'
 }
 
